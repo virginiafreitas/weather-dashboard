6ae7ebea81daad862fb173407556cb14a40f88e8
virginiafreitas <virginiacdefreitas@gmail.com>
2023-07-14 19:14:54 -0700

    cleaned HTML comments

--- assets/script.js
+++ assets/script.js
@@ -6,17 +6,15 @@ function displayWeather(){
         //var to store API URL with query parameters concatenated (latitude, longitude, var weatherApiKey)
         var weatherApi = "https://api.openweathermap.org/data/2.5/weather?q="+formSearch.value+"&appid="+weatherApiKey+"&units=imperial"  
         // variable empty array to be filled with the desired city
-        var cities = []
-
+        
         var forecastApi = "https://api.openweathermap.org/data/2.5/forecast?q="+formSearch.value+"&appid="+weatherApiKey+"&units=imperial"
         
-
         // fetch to test the API key with the current day weather
         fetch(weatherApi)
-            .then(function(response){
+        .then(function(response){
                 return response.json();
-            })
-            .then(function(data){
+        })
+        .then(function(data){
                 console.log(data);
                 var selectedCity = document.getElementById("selected-city")
                 selectedCity.textContent = "Today in: " + data.name
@@ -30,46 +28,42 @@ function displayWeather(){
                 currentHumidity.textContent = "Humidity: " + data.main.humidity + "%"
                 var currentSymbol = document.getElementById("current-symbol")
                 currentSymbol.src= "https://openweathermap.org/img/wn/"+data.weather[0].icon+"@2x.png"
-
         })
         
         // fetch to test the API key with the 5 day forecast
         fetch(forecastApi)
-            .then(function(response){
+        .then(function(response){
                 return response.json();
-            })
-            .then(function(data){
+        })
+        .then(function(data){
                 console.log(data);
-        var forecastDate = document.querySelectorAll(".date")
-        var forecastTemp = document.querySelectorAll(".forecast-temp")
-        var forecastWind = document.querySelectorAll(".forecast-wind")
-        var forecastHumidity = document.querySelectorAll(".forecast-humidity")
-        var forecastSymbol = document.querySelectorAll("#forecast-symbol")
-
-        for (var i=2, j=0; i<data.list.length; i=i+8, j++) {
-                console.log(data.list[i])
-                var forecastData = data.list[i]
-                forecastDate[j].textContent= dayjs.unix(forecastData.dt).format("MM/DD/YYYY")
-                forecastTemp[j].textContent = "Temp: " + forecastData.main.temp + "°F"
-                forecastWind[j].textContent = "Wind: " + forecastData.wind.speed + " MPH"
-                forecastHumidity[j].textContent = "Humidity: " + forecastData.main.humidity + "%"
-                forecastSymbol[j].src= "https://openweathermap.org/img/wn/"+forecastData.weather[0].icon+"@2x.png"
-        }
-            })
-
-
-
-
-
+                var forecastDate = document.querySelectorAll(".date")
+                var forecastTemp = document.querySelectorAll(".forecast-temp")
+                var forecastWind = document.querySelectorAll(".forecast-wind")
+                var forecastHumidity = document.querySelectorAll(".forecast-humidity")
+                var forecastSymbol = document.querySelectorAll("#forecast-symbol")
+                
+                for (var i=2, j=0; i<data.list.length; i=i+8, j++) {
+                        console.log(data.list[i])
+                        var forecastData = data.list[i]
+                        forecastDate[j].textContent= dayjs.unix(forecastData.dt).format("MM/DD/YYYY")
+                        forecastTemp[j].textContent = "Temp: " + forecastData.main.temp + "°F"
+                        forecastWind[j].textContent = "Wind: " + forecastData.wind.speed + " MPH"
+                        forecastHumidity[j].textContent = "Humidity: " + forecastData.main.humidity + "%"
+                        forecastSymbol[j].src= "https://openweathermap.org/img/wn/"+forecastData.weather[0].icon+"@2x.png"
+                }
+        })
 }
-
 searchButton.addEventListener("click", displayWeather)
 
 
-
-
-
-
+var cities = []
+for (var i=0; i<9; i++) {
+cities[i] = cities.push(selectedCity)
+var searchLinks = document.querySelectorAll(".button")
+searchLinks[j].textContent =  cities[i]
+console.log(cities)
+}
 
 
 
